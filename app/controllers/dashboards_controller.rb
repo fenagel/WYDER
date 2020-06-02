611fb7096a18976e7b2d9a84451d45c95570c226
Lea Grieder <lea@grieder.org>
2020-06-02 17:53:47 +0200

    dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,9 @@
 class DashboardsController < ApplicationController
   def index
-
+    @user = current_user
+    @bookmarks = Bookmark.all
+    @programs = Program.all
+    @subjects = Subject.all
+    @universities = University.all
   end
 end
