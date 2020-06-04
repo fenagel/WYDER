4ed95a8b5d912315fbd3ac3e4ac9774bc26431a7
Felix Nagel <felixna7@gmail.com>
2020-06-04 14:10:43 +0200

    new show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_01_152709) do
+ActiveRecord::Schema.define(version: 2020_06_04_101747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2020_06_01_152709) do
     t.bigint "subject_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "application_link"
     t.index ["subject_id"], name: "index_programs_on_subject_id"
     t.index ["university_id"], name: "index_programs_on_university_id"
   end
