c75dd4e99ff00382bd9d2de832a83efbf595590e
Felix Nagel <felixna7@gmail.com>
2020-06-08 12:38:08 +0200

    searchbar

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -3,7 +3,7 @@ class ProgramsController < ApplicationController
 
   def index
     @programs = Program.all
-
+raise
     if params[:subject].present?
       sql_query = " \
         subjects.name @@ :query \
