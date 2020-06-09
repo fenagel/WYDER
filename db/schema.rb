825516f38cbd2c6eeac27488d63fcf5f6f2d50dc
Alexander Weiss <weiss.alexander.96@gmail.com>
2020-06-09 16:46:35 +0200

    Homepage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_09_082316) do
+ActiveRecord::Schema.define(version: 2020_06_09_122111) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -93,7 +93,6 @@ ActiveRecord::Schema.define(version: 2020_06_09_082316) do
     t.bigint "university_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
     t.string "provider"
     t.string "uid"
     t.string "facebook_picture_url"
@@ -101,6 +100,7 @@ ActiveRecord::Schema.define(version: 2020_06_09_082316) do
     t.string "last_name"
     t.string "token"
     t.datetime "token_expiry"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["university_id"], name: "index_users_on_university_id"
